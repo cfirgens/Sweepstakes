172c620051bf2d0e9e19f9a28160ceafbb9fa569
cfirgens <firgenscj@gmail.com>
2019-07-24 13:33:19 -0500

    added in logic for print contestant and pick winner methods in sweepstakes

--- Sweepstakes.cs
+++ Sweepstakes.cs
@@ -1,53 +1,46 @@
-﻿using MailKit.Net.Smtp;
-using MailKit;
-using MimeKit;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Sweepstakes
 {
     internal class Sweepstakes
     {
         //member variables
+        List<Contestant> contestants = new List<Contestant>();
+        string name;
 
         //constructor
-        private Sweepstakes(string name)
+        public Sweepstakes(string name)
         {
+            this.name = name;
         }
 
         //methods
         public void RegisterContestant(Contestant contestant)
         {
+            contestants.Add(contestant);
         }
 
         public void PrintContestantInfo(Contestant contestant)
         {
+            foreach (Contestant contestant in contestants)
+            { 
+                Console.WriteLine("Contestant name: " + contestant.FirstName + " " + contestant.LastName + " \nContestant email: " + contestant.Email + " \nContestant Registration Number: " + contestant.RegistrationNumber);
+            }
+        }
+        
+        public string PickWinner()
+        {
+            int index;
+            Contestant winner;
+            Random random = new Random();
+            index = random.Next(1, contestants.Count - 1);
+            winner = contestants[index];
+            return winner.FirstName + " " + winner.LastName;
         }
 
-        // email winner
-
-        public void EmailWinner(string firstName, string lastName, string email)
-        {
-            var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("Sweepstakes Manager", "Sweepstakes@sweep.com"));
-            message.To.Add(new MailboxAddress(firstName + lastName, email));
-            message.Subject = "Congratulations! You won the sweepstakes";
-
-            message.Body = new TextPart("plain")
-            {
-                Text = @"Dear contestant,
-You have won the sweepstakes, congratulations! Please contact the sweepstakes manager to claim your prize.
-Sweepstakes manager can be reached at (414) 999-6666. Please have your name, email and registration number when you call
-
---Sweepstakes team"
-            };
-
-            using (var client = new SmtpClient())
-            {
-                //accepting all SSL certs
-                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
-
-                client.Send(message);
-                client.Disconnect(true);
-            }
-        } 
     }
 }
