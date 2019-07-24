7ccb22cfc9bbdf3dc221323e6c84ad54b12d1009
cfirgens <firgenscj@gmail.com>
2019-07-24 08:00:21 -0500

    added in contestant methods to get new contestant and registation number

--- Contestant.cs
+++ Contestant.cs
@@ -14,13 +14,40 @@ namespace Sweepstakes
         string email;
         int registrationNumber;
 
+        //properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int RegistrationNumber { get; set; }
+
         //constructor
-        Contestant (string firstName, string lastName, string email, int registrationNumber)
+        public Contestant(string firstName, string lastName, string email, int registrationNumber)
         {
             this.firstName = firstName;
             this.lastName = lastName;
             this.email = email;
             this.registrationNumber = registrationNumber;
         }
+
+        //methods
+
+        public void GetNewContestant()
+        {
+            Console.WriteLine("What is the contestants first name?");
+            FirstName = Console.ReadLine();
+            Console.WriteLine("What is the contestants last name?");
+            LastName = Console.ReadLine();
+            Console.WriteLine("What is the contestants email address?");
+            Email = Console.ReadLine();
+            RegistrationNumber = GetRegistrationNumber();
+        }
+
+        private int GetRegistrationNumber()
+        {
+            Random random = new Random();
+            return random.Next(1, 1000);
+
+        }
     }
 }
+
